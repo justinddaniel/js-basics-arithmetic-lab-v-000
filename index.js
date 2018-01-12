27b96b27510efe6558c87ca098715f8b8bb10140
Justin Daniel <justinddaniel@gmail.com>
2018-01-12 04:56:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,4 @@ let randomNumber = Math.random() * 20
 
 let randomInteger = Math.floor(randomNumber)
 
-let randomUserID = Math.ceil(Math.random * 20) + 1000000000
+let randomUserID = Math.ceil(Math.random() * 20) + 1000000000
